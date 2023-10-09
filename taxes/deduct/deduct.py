3451d508c262024958083a21f3c4fb1289e2d4ed
sizu <sizu@inl.ics.keio.ac.jp>
2023-10-09 16:19:03 +0900

    add function to calculate resident taxes

--- taxes/deduct/deduct.py
+++ taxes/deduct/deduct.py
@@ -2,6 +2,18 @@ import yaml
 
 
 def load_income_yml(income_yml_path):
+    """[load income.yml files and calculate total income and social insurance of year]
+
+    Args:
+        income_yml_path (string): file path to income.yml
+
+    Returns:
+        int, int: return total income and social insurance of year.
+                  if some values in income.yml are missing, 
+                  total income and social insurance of year will be estimated 
+                  using average of the values provided.
+    """
+
     total_income = 0
     total_bonus = 0
     total_social_insurance = 0
@@ -39,7 +51,16 @@ def load_income_yml(income_yml_path):
         return total_income + total_bonus, total_social_insurance
 
 def load_tax_yml(tax_yml_path):
-    with open('yml/tax.yml', 'r') as tax_yml, open('yml/income.yml', 'r') as income_yml:
+    """[load tax.yml files and return dict of tax and additional_payment]
+
+    Args:
+        tax_yml_path (string): file path to tax.yml
+
+    Returns:
+        dict, dict: return tax and additional_payment in tax.yml
+    """
+
+    with open(tax_yml_path, 'r') as tax_yml:
         tax = yaml.safe_load(tax_yml)
 
         additonal_payment = None
@@ -104,6 +125,12 @@ def calc_income_tax(income, rules_for_income_tax):
     print('out of rules for income tax: please add condition in calc_income_tax()')
     exit(1)
 
+def calc_resident_tax(income, rules_for_resident_tax):
+
+    return income * rules_for_resident_tax['tax_ratio'] + \
+            rules_for_resident_tax['tax_per_capita']['prefectual_tax'] + \
+            rules_for_resident_tax['tax_per_capita']['municipal_tax']
+
 
 if __name__ == "__main__":
     income, social_insurance = load_income_yml('yml/income.yml')
@@ -130,6 +157,9 @@ if __name__ == "__main__":
     print('taxable income (課税所得): ' + str(taxable_income))
 
     income_tax = calc_income_tax(taxable_income, tax['rules_for_income_tax'])
-    print('income_tax (所得税): ' + str(income_tax))
+    print('income tax (所得税): ' + str(income_tax))
+
+    resident_tax = calc_resident_tax(taxable_income, tax['rules_for_resident_tax'])
+    print('resident tax (住民税): ' + str(resident_tax))
 
 
